329b05336e6f44e590869f6b707729ef084a8834
Marc <marcsanchez@me.com>
2022-03-14 20:57:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const newHeader = document.createElement('h1');
+const h1 = newHeader;
+
+main.remove();
+body.appendChild(h1);
+h1.setAttribute('id', 'victory');
+h1.innerHTML = "Marc is the champion";
